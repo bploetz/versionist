69bfd0090ff023868a20c160f464f275de8d721e
Brian Ploetz <bploetz@savewave.com>
2011-12-09 16:47:33 -0500

    Railtie class was closed too early

--- lib/versionist/railtie.rb
+++ lib/versionist/railtie.rb
@@ -3,11 +3,11 @@ require 'rails'
 module Versionist
   class Railtie < Rails::Railtie
     config.versionist = ActiveSupport::OrderedOptions.new
-  end
 
-  initializer 'versionist.configure' do |app|
-    Versionist.configure do |config|
-      config.vendor_name = app.config.versionist[:vendor_name]
+    initializer 'versionist.configure' do |app|
+      Versionist.configure do |config|
+        config.vendor_name = app.config.versionist[:vendor_name]
+      end
     end
   end
 end
