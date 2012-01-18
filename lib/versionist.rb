9e4affe049195049c1ea5da7a88ce42d507f19f8
Brian Ploetz <bploetz@savewave.com>
2012-01-18 15:22:28 -0500

    Moving generators to lib/generators/versionist where Rails expects them

--- lib/versionist.rb
+++ lib/versionist.rb
@@ -4,9 +4,9 @@ module Versionist
   extend ActiveSupport::Autoload
 
   autoload :Configuration
-  autoload :NewApiVersionGenerator, "generators/new_api_version/new_api_version_generator"
-  autoload :NewControllerGenerator, "generators/new_controller/new_controller_generator"
-  autoload :NewPresenterGenerator, "generators/new_presenter/new_presenter_generator"
+  autoload :NewApiVersionGenerator, "generators/versionist/new_api_version/new_api_version_generator"
+  autoload :NewControllerGenerator, "generators/versionist/new_controller/new_controller_generator"
+  autoload :NewPresenterGenerator, "generators/versionist/new_presenter/new_presenter_generator"
   autoload :VersioningStrategy, "versionist/versioning_strategy"
   autoload :Middleware
   autoload :Routing
