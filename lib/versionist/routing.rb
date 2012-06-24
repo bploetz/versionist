4c38053cd1f883dc49244c66d75afc1582533f15
Paul Alexander <paul@xheo.com>
2012-06-23 22:18:54 -0700

    Accept custom :as parameter for path strategy

--- lib/versionist/routing.rb
+++ lib/versionist/routing.rb
@@ -36,7 +36,8 @@ module Versionist
       path = Versionist::VersioningStrategy::Path.new(config)
       # Use the :as option and strip out non-word characters from the path to avoid this:
       # https://github.com/rails/rails/issues/3224
-      route_hash = {:module => config[:module], :as => config[:path].gsub(/\W/, '_')}
+      route_hash = {:module => config[:module] }
+      route_has[:as] = config[:as] || config[:path].gsub(/\W/, '_') unless config[:no_as]
       route_hash.merge!({:defaults => config[:defaults]}) if config.has_key?(:defaults)
       namespace(config[:path], route_hash, &block)
       if path.default?
