4fef3d9e7773b920a0d59008928ea6d426b16a9d
Chris Altman <chris@altonymous.com>
2013-08-24 23:11:05 -0400

    Generate all strategies, but only generate the header if it is NOT the default.

--- lib/versionist/routing.rb
+++ lib/versionist/routing.rb
@@ -29,13 +29,10 @@ module Versionist
       raise ArgumentError, "you must specify :module in configuration Hash passed to api_version" if !config.has_key?(:module)
       raise ArgumentError, ":defaults must be a Hash" if config.has_key?(:defaults) && !config[:defaults].is_a?(Hash)
 
-      if config.has_key?(:default) && config[:default]
-        configure_default(config, &block) if config.has_key?(:default) && config[:default]
-      else
-        configure_header(config, &block) if config.has_key?(:header)
-        configure_path(config, &block) if config.has_key?(:path)
-        configure_parameter(config, &block) if config.has_key?(:parameter)
-      end
+      configure_default(config, &block) if config.has_key?(:default) && config[:default]
+      configure_path(config, &block) if config.has_key?(:path)
+      configure_parameter(config, &block) if config.has_key?(:parameter)
+      configure_header(config, &block) if config.has_key?(:header) && !config.has_key?(:default) && !config[:default]
     end
 
 
